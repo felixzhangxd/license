d6aab433ceb24feba04490077f1cb77f01653744
felix <zhang.xiaodong151@chinaredstar.com>
2015-12-22 17:12:06 +0800

    tmp

--- src/main/java/com/firebugsoft/license/util/ImageUtil.java
+++ src/main/java/com/firebugsoft/license/util/ImageUtil.java
@@ -1,4 +1,47 @@
 package com.firebugsoft.license.util;
 
-public class ImageUtil {
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public final class ImageUtil {
+    private ImageUtil() {}
+
+    /** 图片二值化 */
+    public static BufferedImage imageBinary(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage binary = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rgb = image.getRGB(i, j);
+                binary.setRGB(i, j, rgb);
+            }
+        }
+        return binary;
+    }
+
+    /** 图片灰度化 */
+    public static BufferedImage imageGray(BufferedImage image) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        BufferedImage gray = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rgb = image.getRGB(i, j);
+                gray.setRGB(i, j, rgb);
+            }
+        }
+        return gray;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedImage image = ImageIO.read(new File("E:\\duck.jpg"));
+        BufferedImage binary = imageBinary(image);
+        ImageIO.write(binary, "jpg", new File("E:\\duck-binary.jpg"));
+        BufferedImage gray = imageGray(image);
+        ImageIO.write(gray, "jpg", new File("E:\\duck-gray.jpg"));
+    }
+
 }
